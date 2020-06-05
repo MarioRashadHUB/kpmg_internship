e714858e7c69888b8e08f46a188b55e580b1319a
Mario <mariocanedit1@gmail.com>
2020-06-05 13:20:19 -0400

    removed first row from all datasets allowing col titles properly display

---
+++ kpmg_data_quality_assessment.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  5 13:01:18 2020
+
+@author: Mario
+"""
+
+import pandas as pd
+
+# imports all three datasets
+df_demograph = pd.read_csv('cust_demograph.csv')
+df_address = pd.read_csv('cust_demograph.csv')
+df_trans = pd.read_csv('cust_demograph.csv')
