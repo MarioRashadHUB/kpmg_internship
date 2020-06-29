38dd4b9c604823d21aaa596e21d4f1111bc981c1
MTSS <blackiviagic@gmail.com>
2020-06-29 13:02:00 -0400

    started eda

--- Task 2 (Data Insights)/powerpoint_assessment.py
+++ Task 2 (Data Insights)/powerpoint_assessment.py
@@ -16,3 +16,22 @@ pd.options.display.max_rows = None
 # importing all new recieved data
 df = pd.read_csv('new_cust_list.csv')
 
+df.columns
+df.owns_car.value_counts()
+
+# 25+ bike purchahes within past 3 years
+df['25_plus'] = df['past_3_years_bike_related_purchases'].apply(lambda x: True if (x >= 25 ) else False)
+df['25_plus'].value_counts()
+
+# new df with all customers who have made more then 25 purchases
+df_targetCust = df[df['25_plus'] == True]
+df_targetCust['25_plus'].value_counts()
+
+# removes all cusomers who do not own a car
+df_targetCust = df[df['owns_car'] == 'No']
+df_targetCust.owns_car.value_counts()
+
+
+# removes all customers with no DOB
+df_targetCust.dropna(subset = ["DOB"], inplace=True)
+
